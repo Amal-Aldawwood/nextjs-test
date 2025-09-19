14bae019954b65829f074db061b1524c69e1a77d
Amal <amalaaldaood@gmail.com>
2025-09-19 18:05:02 +0300

    comment 1

---
+++ src/Major/page.jsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+export default function Major() {
+  return (
+    <div>
+      <h1>Major: Computer Science </h1>
+      <h1>In:Umm cdulqura  </h1>
+      <h1>In: Aljammom  </h1>
+      <Link href="/Home">Go to Home</Link>
+
+    </div>
+  );
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h3>Name : Amal Aldawood</h3>
+      <Link href="/Major">Go to Major</Link>
+    </div>
+  );
+}
