51b9ca196086b6c7cc3f3076348faa07c8415384
Amal <amalaaldaood@gmail.com>
2025-09-20 02:03:19 +0300

    تعديل على صفحة التخصص: تصحيح اسم الجامعة

--- src/Major/page.jsx
+++ src/Major/page.jsx
@@ -4,8 +4,8 @@ export default function Major() {
   return (
     <div>
       <h1>Major: Computer Science </h1>
-      <h1>In:Umm cdulqura  </h1>
-      <h1>In: Aljammom  </h1>
+      <h1>Umm Al-Qura University </h1>
+      <h1>In: Al-jammom  </h1>
       <Link href="/Home">Go to Home</Link>
 
     </div>
