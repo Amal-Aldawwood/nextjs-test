3687e09f7d3f27dbb69cb6ee6dccb558a52911e4
Amal <amalaaldaood@gmail.com>
2025-09-20 18:49:00 +0300

    إضافة صفحة major وتصحيح عرض التخصص

---
+++ src/app/major/page.jsx
@@ -0,0 +1,10 @@
+export default function major() {
+  return (
+    <div>
+      <h1>Major: Computer Science</h1>
+      <h2>In: Umm Al-Qura University</h2>
+      <h2>Location: Al-Jummum</h2>
+    </div>
+  );
+}
+ 
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,8 +3,8 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div>
-      <h3>Name : Amal Aldawood</h3>
-      <Link href="/Major">Go to Major</Link>
+      <h1>Name: Amal Aldawood</h1>
+      <Link href="/major">Go to Major</Link>
     </div>
   );
 }
